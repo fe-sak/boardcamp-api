447b15254991a11044dfc0972e269ee49e49fe82
Felipe Sakuma <fycorsak@gmail.com>
2022-03-03 13:39:08 -0300

    feat: get customers

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -1,6 +1,25 @@
 import connection from '../database.js';
 
-export default async function postCustomer(req, res) {
+export async function getCustomers(req, res) {
+  try {
+    if (req.query.cpf) {
+      const cpfPattern = `${req.query.cpf}%`;
+      const queryResult = await connection.query(
+        `SELECT * FROM customers 
+            WHERE cpf LIKE $1`,
+        [cpfPattern]
+      );
+      res.send(queryResult.rows);
+    } else {
+      const queryResult = await connection.query(`SELECT * FROM customers`);
+      res.send(queryResult.rows);
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+}
+
+export async function postCustomer(req, res) {
   const { name, phone, cpf, birthday } = req.body;
 
   try {
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import postCustomer from '../controllers/customersControllers.js';
+import {
+  getCustomers,
+  postCustomer,
+} from '../controllers/customersControllers.js';
 import validateCustomer from '../middlewares/validateCustomerMiddleware.js';
 import validateSchema from '../middlewares/validateSchemaMiddleware.js';
 import customerSchema from '../schemas/customerSchema.js';
 
 const customersRouter = Router();
-
+customersRouter.get('/customers', getCustomers);
 customersRouter.post(
   '/customers',
   validateSchema(customerSchema),
