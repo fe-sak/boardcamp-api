9bf67eb47c791df31f7c67e837b5db5db2529acd
Felipe Sakuma <fycorsak@gmail.com>
2022-03-03 20:59:41 -0300

    refactor: improve readability for validateCustomerPost

--- src/middlewares/validateCustomerPostMiddleware.js
+++ src/middlewares/validateCustomerPostMiddleware.js
@@ -3,12 +3,15 @@ import connection from '../database.js';
 export default async function validateCustomerPost(req, res, next) {
   const { cpf } = req.body;
   try {
-    const customerExists = await connection.query(
+    const queryResult = await connection.query(
       `SELECT * FROM customers 
     WHERE cpf=$1`,
       [cpf]
     );
-    if (customerExists.rows.length !== 0) return res.sendStatus(409);
+
+    const customer = queryResult.rows[0];
+
+    if (customer) return res.sendStatus(409);
   } catch {
     return res.sendStatus(500);
   }
