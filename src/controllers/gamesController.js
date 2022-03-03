b3724d5d1edaa05983973010f769bcdfbdf60515
Felipe Sakuma <fycorsak@gmail.com>
2022-03-03 13:41:06 -0300

    fix: remove parseInt from postGame

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -25,12 +25,10 @@ export async function getGames(req, res) {
 
 export async function postGame(req, res) {
   const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
-  const parsedStockTotal = parseInt(stockTotal);
-  const parsedPricePerDay = parseInt(pricePerDay);
   try {
     await connection.query(
       'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',
-      [name, image, parsedStockTotal, categoryId, parsedPricePerDay]
+      [name, image, stockTotal, categoryId, pricePerDay]
     );
     return res.sendStatus(201);
   } catch {
