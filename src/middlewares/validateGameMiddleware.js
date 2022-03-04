a7b4c79c2c7eb0db848a5ca61d167b82a6c1aa0f
Felipe Sakuma <fycorsak@gmail.com>
2022-03-03 21:08:58 -0300

    refactor: improve readability of validateGame

--- src/middlewares/validateGameMiddleware.js
+++ src/middlewares/validateGameMiddleware.js
@@ -5,18 +5,23 @@ import connection from '../database.js';
 export default async function validateGame(req, res, next) {
   const { name, categoryId } = req.body;
   try {
-    const gameExists = await connection.query(
+    const queryResultGame = await connection.query(
       'SELECT * FROM games WHERE name=$1',
       [name]
     );
-    if (gameExists.rows.length !== 0) return res.sendStatus(409);
 
-    const categoryIdExists = await connection.query(
+    const game = queryResultGame.rows[0];
+
+    if (game) return res.sendStatus(409);
+
+    const queryResultCategory = await connection.query(
       'SELECT id FROM categories WHERE id=$1',
       [categoryId]
     );
-    if (categoryIdExists.rows.length === 0)
-      return res.status(400).send("Category Id doesn't exist");
+
+    const category = queryResultCategory.rows[0];
+
+    if (!category) return res.status(400).send("Category Id doesn't exist.");
   } catch {
     return res.sendStatus(500);
   }
