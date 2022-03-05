d60a6cd23fcf03d28f005731d6b080c9e2301b6f
Felipe Sakuma <fycorsak@gmail.com>
2022-03-05 16:08:07 -0300

    chore: remove unnecessary import

--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -4,7 +4,6 @@ import {
   readRentals,
   returnRental,
 } from '../controllers/rentalsController.js';
-import { validateCustomerById } from '../middlewares/validateCustomerMiddlewares.js';
 import {
   validateRentalCreate,
   validateRentalRead,
