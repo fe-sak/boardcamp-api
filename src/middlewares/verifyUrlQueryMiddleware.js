8720553989100caad832508cb51d889d77d475c0
Felipe Sakuma <fycorsak@gmail.com>
2022-03-06 18:06:06 -0300

    feat: add order option to all get routes

--- src/middlewares/verifyUrlQueryMiddleware.js
+++ src/middlewares/verifyUrlQueryMiddleware.js
@@ -1,12 +1,21 @@
 export default async function verifyUrlQuery(req, res, next) {
-  const { offset, limit } = req.query;
+  const { offset, limit, order, desc } = req.query;
+  console.log(order);
   let sqlQueryString = '';
   if (offset) {
     sqlQueryString += `OFFSET ${offset} `;
   }
 
   if (limit) {
-    sqlQueryString += `LIMIT ${limit}`;
+    sqlQueryString += `LIMIT ${limit} `;
+  }
+
+  if (order) {
+    sqlQueryString += `ORDER BY "${order}" `;
+
+    if (desc === 'true') {
+      sqlQueryString += `DESC `;
+    }
   }
   res.locals.sqlQueryOptions = sqlQueryString;
   next();
